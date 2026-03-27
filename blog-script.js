f8525998197bde3a2694c194b058910ccc111220
ditsel <alinadicel92@gmail.ru>
2026-03-27 23:28:15 +0700

    lesson_6

--- blog-script.js
+++ blog-script.js
@@ -1,10 +1,28 @@
 const buttonEditName = 'button-edit';
 const addingFormName = '.adding-form';
-const buttonCancelName = 'article-cancel-button'
+const buttonCancelName = 'article-cancel-button';
+const articleListName = '.article-grid';
+const articleCardTemplateId = 'article-card-tmp';
+const addArticleButtonId = 'article-post-button';
+const openStatBtnId = 'button-stat';
+const dialogId = 'confirmDialog';
+const articleCardClassName ='article-item';
+const articleStatisticName='.article-statistic';
+const articleCountElementId = 'article-count';
+
 
 const postBtn = document.getElementById(buttonEditName);
 const cancelBtn = document.getElementById(buttonCancelName);
 const addingForm = document.querySelector(addingFormName);
+const articleList = document.querySelector(articleListName);
+const articleCardTemplate = document.getElementById(articleCardTemplateId);
+const addArticleButton = document.getElementById(addArticleButtonId);
+const openStatBtn = document.getElementById(openStatBtnId);
+const dialog = document.getElementById(dialogId);
+const articles = document.getElementsByClassName(articleCardClassName);
+const articleStatistic = document.querySelector(articleStatisticName);
+const articleCountElement = document.getElementById(articleCountElementId);
+const closeStatBtn = dialog.querySelector('[data-close]');
 
 
 function addPostButtonListener() {
@@ -25,6 +43,7 @@ function addPostButtonListener() {
 
     function toHideForm() {
         isEdit = false;
+        addingForm.reset();
         addingForm.setAttribute('data-hidden', '');
     }
 
@@ -32,48 +51,46 @@ function addPostButtonListener() {
 addPostButtonListener();
 
 
+function handleAddArticleForm() {
+    const articleTitleField = addingForm.elements['title'];
+    const articleDescriptionField = addingForm.elements['paper-text'];
+    
+    if (!addingForm.checkValidity()) {
+        return;
+    }
 
-const articleListName = '.article-grid';
-const articleCardTemplateId = 'article-card-tmp';
-const articleList = document.querySelector(articleListName);
-const articleCardTemplate = document.getElementById(articleCardTemplateId);
-
-const addArticleButtonId = 'article-post-button';
-const addArticleButton = document.getElementById(addArticleButtonId);
-const addForm = document.querySelector('.adding-form');
-
-addForm?.addEventListener('submit', (event) => {
-    event.preventDefault();
     const clonedTemplateNode = articleCardTemplate.content.cloneNode(true);
 
+    clonedTemplateNode.querySelector('h4').textContent = articleTitleField.value;
+    
     articleList.append(clonedTemplateNode);
-})
+    addingForm.reset();
+}
 
 
-const openStatBtnId = 'button-stat';
-const dialogId = 'confirmDialog';
-const articleCardClassName ='article-item';
-const articleStatisticName='.article-statistic';
-const articleCountElementId = 'article-count'
+function event_handlers(){
+    addingForm?.addEventListener('submit', (event) => {
+        event.preventDefault();
+        handleAddArticleForm();
+    })
+    articleList.addEventListener('click', (event) =>{
+    if (event.target.getAttribute('data-delete') == ''){
+        event.target.parentElement.remove();
+    }
+    })
 
-const openStatBtn = document.getElementById(openStatBtnId);
-const dialog = document.getElementById(dialogId);
-const articles = document.getElementsByClassName(articleCardClassName);
-const articleStatistic = document.querySelector(articleStatisticName);
-const articleCountElement = document.getElementById(articleCountElementId);
+    openStatBtn.addEventListener('click', () => {
+        const articlesCount = articles.length
+        articleCountElement.textContent = articlesCount;
+        dialog.showModal();
+    });
+    closeStatBtn.addEventListener('click', () => {
+        dialog.close('x');
+    });
 
-openStatBtn.addEventListener('click', () => {
-    const articlesCount = articles.length
-    articleCountElement.textContent = articlesCount;
-    dialog.showModal();
-});
+    dialog.addEventListener('click', (e) => {
+        if (e.target === dialog) dialog.close('backdrop');
+    });
 
-
-const closeStatBtn = dialog.querySelector('[data-close]');
-closeStatBtn.addEventListener('click', () => {
-    dialog.close('x');
-});
-
-dialog.addEventListener('click', (e) => {
-    if (e.target === dialog) dialog.close('backdrop');
-});
+}
+event_handlers();
