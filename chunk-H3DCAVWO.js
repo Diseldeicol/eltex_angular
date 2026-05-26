568c36e24813eea5b727e97b4204465d1d79190e
Diseldeicol <alinadicel92@mail.ru>
2026-05-26 04:41:16 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Diseldeicol/eltex_angular/commit/78dc6ba9acf35d16582b96c1905250038663c737
    GitHub Actions build: https://github.com/Diseldeicol/eltex_angular/actions/runs/26432647351

---
+++ chunk-H3DCAVWO.js
@@ -0,0 +1,1 @@
+import{V as e}from"./chunk-B33IZS34.js";var t=new e("ARTICLES_SERVICE");export{t as a};
--- chunk-LKWNWB4P.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,useBackendApi:!1,apiUrl:"",fileBaseUrl:"",mockImageUrl:"images/noname_photo.png"};export{e as a};
